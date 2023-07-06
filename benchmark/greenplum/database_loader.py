bae3fc7a0f327728d890c27215add8ae797d83f1
Stanislav Bogatskiy <sbkubric@gmail.com>
2023-07-06 11:28:22 +0300

    feat/naming (#30)
    * feat/ch
    
    * fix: namings
    
    ---------
    
    Co-authored-by: serg <sergeusprecious@gmail.com>

--- benchmark/greenplum/database_loader.py
+++ benchmark/greenplum/database_loader.py
@@ -1,10 +1,8 @@
-from abc import ABC
-from abc import abstractmethod
+from abc import ABC, abstractmethod
 from contextlib import contextmanager
 
 import psycopg2
-
-from db_tester.utils import generate_random_data
+from benchmark.utils import generate_random_data
 from logger import logger
 from timer import Timer
 
@@ -17,7 +15,7 @@ class DatabaseLoader(ABC):
 
     @contextmanager
     def _connection(self):
-        """ Контекстный менеджер подключения к БД."""
+        """Контекстный менеджер подключения к БД."""
         conn = psycopg2.connect(**self.params)
 
         try:
@@ -39,36 +37,45 @@ class DatabaseLoader(ABC):
 
 
 class DatabaseLoaderRead(DatabaseLoader):
-    def __init__(self, sql, params: dict, rows: int = 10_000_000, batch_size: int = 100_000):
+    def __init__(
+        self, sql, params: dict, rows: int = 10_000_000, batch_size: int = 100_000
+    ):
         self.sql = sql
-        self.params_sql = {'offset': 0, 'limit': batch_size}
+        self.params_sql = {"offset": 0, "limit": batch_size}
         super().__init__(params, rows, batch_size)
 
     @Timer(name="read", logger=logger.info)
     def _select(self, curs):
         curs.execute(self.sql, self.params_sql)
         result = curs.fetchall()
-        #logger.info(f"Count read rows: {len(result)}")
+        # logger.info(f"Count read rows: {len(result)}")
 
     def _exec(self, conn, curs):
         offset = 0
-        for _ in range(int(self.rows/self.batch_size)):
-            self.params_sql['offset'] = offset
+        for _ in range(int(self.rows / self.batch_size)):
+            self.params_sql["offset"] = offset
             self._select(curs)
             offset += self.batch_size
 
 
 class DatabaseLoaderWrite(DatabaseLoader):
-    def __init__(self, file_name, table_name, params: dict, rows: int = 10_000_000, batch_size: int = 100_000):
+    def __init__(
+        self,
+        file_name,
+        table_name,
+        params: dict,
+        rows: int = 10_000_000,
+        batch_size: int = 100_000,
+    ):
         self.file_name = file_name
         self.table_name = table_name
         super().__init__(params, rows, batch_size)
-    def _gen_csv(self, file_name, batch):
-        with open(file_name, 'w') as f:
-            f.write('id,user_id,filem_id,timestamp\n')
-            lines = [','.join([str(v) for _, v in row.items()]) for row in batch]
-            f.writelines('\n'.join(lines))
 
+    def _gen_csv(self, file_name, batch):
+        with open(file_name, "w") as f:
+            f.write("id,user_id,filem_id,timestamp\n")
+            lines = [",".join([str(v) for _, v in row.items()]) for row in batch]
+            f.writelines("\n".join(lines))
 
     @Timer(name="write", logger=logger.info)
     def _insert(self, conn, curs, copy_statement, file):
