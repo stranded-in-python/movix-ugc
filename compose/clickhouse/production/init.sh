3586c906013c975d7c2d9531e4da2ed6de2f01fc
Viktor-Gostyaikin <gostyaikin@yandex.ru>
2023-07-06 15:48:19 +0300

    wip: start clickhouse

--- compose/clickhouse/production/init.sh
+++ compose/clickhouse/production/init.sh
@@ -10,9 +10,9 @@ new_string='<access_management>1</access_management>'
 # Заменяем строку в файле с помощью sed
 sed -i "s|$old_string|$new_string|g" "$file_path"
 
-sql_user="CREATE USER IF NOT EXISTS $CH_USERNAME IDENTIFIED WITH PLAINTEXT_PASSWORD BY '$CH_PASSWORD';"
-sql_db="CREATE DATABASE IF NOT EXISTS $CH_DATABASE;"
-sql_grant="GRANT ALL PRIVILEGES ON $CH_DATABASE.* TO $CH_USERNAME;"
+sql_user="CREATE USER IF NOT EXISTS $CLICKHOUSE_USERNAME IDENTIFIED WITH PLAINTEXT_PASSWORD BY '$CLICKHOUSE_PASSWORD';"
+sql_db="CREATE DATABASE IF NOT EXISTS $CLICKHOUSE_DB;"
+sql_grant="GRANT ALL PRIVILEGES ON $CLICKHOUSE_DB.* TO $CLICKHOUSE_USERNAME;"
 
 clickhouse-client -q "SYSTEM RELOAD CONFIG;"
 clickhouse-client -q "$sql_user"
